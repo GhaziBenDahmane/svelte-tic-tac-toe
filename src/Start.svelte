94ac5c0d16db33f9d1947af7842abb08e983adc8
Ghazi Ben Dahmane <ghazi.ben.dahmane@gmail.com>
2020-12-24 15:30:44 +0100

    win condition can't exceed grid size

--- src/Start.svelte
+++ src/Start.svelte
@@ -1,6 +1,11 @@
 <script>
     export let winCondition = 3;
     export let gridSize = 3;
+    $: {
+        if (winCondition > gridSize) {
+            winCondition = gridSize;
+        }
+    }
 </script>
 
 <style>
@@ -13,11 +18,11 @@
 
 <h2>Welcome to the best tic tac toe game</h2>
 <p>Board Size?</p>
-<input type="number" bind:value={gridSize} />
-<input type="range" bind:value={gridSize} />
+<input type="number" bind:value={gridSize} min="3" max="10" />
+<input type="range" bind:value={gridSize} min="3" max="10" />
 <br />
 <p>How many in a row to win?</p>
-<input type="number" bind:value={winCondition} />
-<input type="range" bind:value={winCondition} />
+<input type="number" bind:value={winCondition} min="3" max={gridSize} />
+<input type="range" bind:value={winCondition} min="3" max={gridSize} />
 <br />
 <button on:click>Start</button>
